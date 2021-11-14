9f5b4654b9f98aa3c6735232ef1bdfa1ce7a4590
Angeredspy <devygraham@gmail.com>
2021-11-14 11:26:34 -0500

    Adds typescript to catch errors while developing

--- index.ts
+++ index.ts
@@ -2,16 +2,26 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface toDo {
+  id: number;
+  completed: boolean;
+  title: string;
+}
+
 axios.get(url).then((response) => {
-  const toDo = response.data;
+  const toDo = response.data as toDo;
 
   const id = toDo.id;
   const completed = toDo.completed;
   const title = toDo.title;
 
+  logToDo(id, completed, title);
+});
+
+const logToDo = (id: number, completed: boolean, title: string) => {
   console.log(`
         The item with the ID: ${id}
         Has a title of: ${title}
         Is it finished? ${completed}
     `);
-});
+};
