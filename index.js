cda1b6bb4656722d263b575f760f222fca48ef94
Angeredspy <devygraham@gmail.com>
2021-11-14 10:58:36 -0500

    ts-node reinstall

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var toDo = response.data;
+    var id = toDo.id;
+    var completed = toDo.completed;
+    var title = toDo.title;
+    console.log("\n        The item with the ID: " + id + "\n        Has a title of: " + title + "\n        Is it finished? " + completed + "\n    ");
 });
--- index.ts
+++ index.ts
@@ -3,5 +3,15 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const toDo = response.data;
+
+  const id = toDo.id;
+  const completed = toDo.completed;
+  const title = toDo.title;
+
+  console.log(`
+        The item with the ID: ${id}
+        Has a title of: ${title}
+        Is it finished? ${completed}
+    `);
 });
